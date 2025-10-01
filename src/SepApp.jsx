17355b29a0fe99d937cfc7f35ab7241b23b32415
alisho0 <alejorea2015@gmail.com>
2025-09-30 22:18:40 -0300

    Configuración de tailwind y entendiendo como funciona el framework

---
+++ src/SepApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function SepApp() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-linear-to-r from-cyan-100 to-cyan-700 ">
+        Hello worldsss!
+      </h1>
+      <p className='text-joder bg-amber-300'>Texto de ejemplo</p>
+    </>
+  )
+}
+
+export default SepApp
