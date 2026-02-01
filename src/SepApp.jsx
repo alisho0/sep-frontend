8b8c4ad16e8bc14284ff159d86715293b6479f93
Alejandro <alejorea2015@gmail.com>
2026-02-01 12:28:30 -0300

    Configuración de rutas privadas según roles y token.

--- src/SepApp.jsx
+++ src/SepApp.jsx
@@ -14,23 +14,31 @@ import { Usuarios } from './features/usuarios/pages/Usuarios'
 import { Configuracion } from './components/configuracion/Configuracion'
 import { MenuMaestro } from './pages/MenuMaestro'
 import { GradosAsignados } from './features/grados/pages/GradosAsignados'
+import { PrivateRoute } from './routes/PrivateRoute'
 function SepApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>} />
-        <Route element={<Layout />}>
-          <Route path='/menu' element={<Menu/>}/>
-          <Route path='/alumnos' element={<Alumnos />} /> 
-          <Route path='/alumnos/:id' element={<DetalleAlumno />} />
-          <Route path='/discapacidades' element={<Discapacidades />} />
-          <Route path='/grados' element={<Grados/>}/>
-          <Route path='/grados/:id' element={<GradoDetalle />} />
-          <Route path='/grados/:id/ciclo/:cicloId' element={<GradoCicloDetalle />} />
-          <Route path='/usuarios' element={<Usuarios />} />
-          <Route path='/configuracion' element={<Configuracion />} />
-          <Route path='/menu-maestro' element={<MenuMaestro/>} />
-          <Route path='/grados-asignados' element={<GradosAsignados />} />
+        <Route element={<PrivateRoute />}>
+          <Route element={<Layout />}>
+            <Route path='/alumnos' element={<Alumnos />} /> 
+            <Route path='/alumnos/:id' element={<DetalleAlumno />} />
+            <Route path='/grados/:id/ciclo/:cicloId' element={<GradoCicloDetalle />} />
+            <Route path='/configuracion' element={<Configuracion />} />
+            <Route path='/menu-maestro' element={<MenuMaestro/>} />
+            <Route path='/grados-asignados' element={<GradosAsignados />} />
+          </Route>
+        </Route>
+
+        <Route element={<PrivateRoute allowedRoles={ ["ADMIN", "DIRECTOR"] } />}>
+          <Route element={<Layout />}>
+            <Route path='/usuarios' element={<Usuarios />} />
+            <Route path='/grados' element={<Grados/>}/>
+            <Route path='/grados/:id' element={<GradoDetalle />} />
+            <Route path='/discapacidades' element={<Discapacidades />} />
+            <Route path='/menu' element={<Menu/>}/>
+          </Route>
         </Route>
       </Routes>
       <ModalGlobal />
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,7 +1,19 @@
+import { jwtDecode } from 'jwt-decode';
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
-export const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+export const PrivateRoute = ({ allowedRoles }) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) return <Navigate to="/" />;
+
+  const decoded = jwtDecode(token);
+  const userRoles = decoded.rol || [];
+
+  // Verificamos si el usuario tiene alguno de los roles permitidos
+  const hasAccess = allowedRoles ? allowedRoles.some(role => userRoles.includes(role)) : true;
+
+  return hasAccess ? <Outlet /> : <Navigate to="/configuracion" />;
+
+
 }
